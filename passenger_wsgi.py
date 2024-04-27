209f4aa516ab45430cc5757ad798fda2eee9ecc4
rajeshbond <115805015+rajeshbond@users.noreply.github.com>
2024-04-28 00:08:04 +0530

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,10 +1,1 @@
-import os
-import sys
-from a2wsgi import ASGIMiddleware
-from app.main import app
 
-
-# Add the directory containing the `passenger_wsgi.py` file to the Python path
-sys.path.insert(0, os.path.dirname(__file__))
-# Wrap the FastAPI app with ASGIMiddleware
-application = ASGIMiddleware(app)
